eafb7147091f82e9b3a69c79e929413fc0ae2ac1
Maksym Zaporozhets <maksimz@default-value.com>
2022-01-19 13:47:36 +0200

    Lesson 15 - Knockout.js UI Components: Extracted form submit into a separate action script

--- app/code/DVCampus/PersonalDiscount/view/frontend/requirejs-config.js
+++ app/code/DVCampus/PersonalDiscount/view/frontend/requirejs-config.js
@@ -1,8 +1,9 @@
 var config = {
     map: {
         '*': {
-            'DVCampus_PersonalDiscount_form': 'DVCampus_PersonalDiscount/js/form',
-            'DVCampus_PersonalDiscount_formOpenButton': 'DVCampus_PersonalDiscount/js/form-open-button'
+            'DVCampus_PersonalDiscount_form': 'DVCampus_PersonalDiscount/js/view/form',
+            'DVCampus_PersonalDiscount_formOpenButton': 'DVCampus_PersonalDiscount/js/view/form-open-button',
+            'DVCampus_PersonalDiscount_submitFormAction': 'DVCampus_PersonalDiscount/js/action/submit-form'
         }
     }
 };
