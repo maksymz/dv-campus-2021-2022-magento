739b04b7e3dae6d0e94a68bcf9742329c5548e16
Maksym Zaporozhets <maksimz@default-value.com>
2022-01-26 17:01:38 +0200

    Lesson 16 - jsLayout and child components: Button now pulls `isModal` configuration from the form component

--- app/code/DVCampus/PersonalDiscount/view/frontend/web/js/view/form-open-button.js
+++ app/code/DVCampus/PersonalDiscount/view/frontend/web/js/view/form-open-button.js
@@ -1,8 +1,10 @@
 define([
     'jquery',
+    'ko',
     'uiComponent',
-    'DVCampus_PersonalDiscount_formSubmitRestrictions'
-], function ($, Component, formSubmitRestrictions) {
+    'DVCampus_PersonalDiscount_formSubmitRestrictions',
+    'DVCampus_PersonalDiscount_form'
+], function ($, ko, Component, formSubmitRestrictions) {
     'use strict';
 
     return Component.extend({
@@ -10,7 +12,20 @@ define([
             template: 'DVCampus_PersonalDiscount/form-open-button'
         },
 
-        formSubmitIsRestricted: formSubmitRestrictions.formSubmitDeniedMessage,
+        /**
+         * Initialize data links (listens/imports/exports/links)
+         * @returns {*}
+         */
+        initLinks: function () {
+            this._super();
+
+            // Check whether it is possible to open the modal - either form is modal or there are any other restrictions
+            this.canShowOpenModalButton = ko.computed(function () {
+                return this.isModal && !formSubmitRestrictions.formSubmitDeniedMessage();
+            }.bind(this));
+
+            return this;
+        },
 
         /**
          * Generate event to open the form
